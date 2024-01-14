0aa9510f9cab0c71190a548cba069a6a5fc0c11b
Arif Hussain <arifh9821@gmail.com>
2024-01-15 00:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
-let ele=document.getElementById("fname")
+let ele=document.getElementById("fname");
 
 function fn(){
 	ele.value=ele.value.toUpperCase();
 }
-ele.addEventListner('mouseleave',fun);
+ele.addEventListner('mouseleave',fn);
